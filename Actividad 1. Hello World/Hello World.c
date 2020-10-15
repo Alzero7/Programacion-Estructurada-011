d21f482bf3ab7ce1514149d948f4beff4c8fcbdb
Alzero7 <jessar.alzero.7@gmail.com>
2020-10-15 13:56:05 -0500

    Actividad 1

---
+++ Actividad 1. Hello World/Hello World.c
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////
+
+///////		Jesus Armando Espino Rodriguez		///////
+
+///////		   Programa 1. Hello World     		///////
+
+////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+	system("color 0B");
+	
+	printf("\n\t Hello World");
+	
+	return 0;
+}
+
+
+
